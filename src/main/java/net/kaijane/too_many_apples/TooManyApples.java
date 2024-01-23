6b5c9bea4abf8e70443e17718346a49e3ae3253f
Kai-Denzel_jane <kaijanedev@icloud.com>
2024-01-23 12:45:28 +0800

    Initial commit

---
+++ src/main/java/net/kaijane/too_many_apples/TooManyApples.java
@@ -0,0 +1,21 @@
+package net.kaijane.too_many_apples;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TooManyApples implements ModInitializer {
+
+	public static final String MOD_ID = "too_many_apples";
+
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Too Many Apples is initializing!");
+
+	}
+}
---
+++ src/main/java/net/kaijane/too_many_apples/TooManyApplesDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kaijane.too_many_apples;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TooManyApplesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
