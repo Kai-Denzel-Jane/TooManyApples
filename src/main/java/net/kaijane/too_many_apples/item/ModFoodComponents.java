7eb6cdba07277eef1a40d3e33d759be82082f65a
Kai-Denzel-Jane <kaijanedev@icloud.com>
2024-03-09 17:38:38 +0800

    Fixed comically large apples

--- src/main/java/net/kaijane/too_many_apples/item/ModFoodComponents.java
+++ src/main/java/net/kaijane/too_many_apples/item/ModFoodComponents.java
@@ -48,12 +48,14 @@ public class ModFoodComponents extends FoodComponents {
 
     // Enchanted Diamond Apple
     public static final FoodComponent ENCHANTED_DIAMOND_APPLE = new FoodComponent.Builder().hunger(4).saturationModifier(0.6f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 3000, 4), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2000, 3), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 3000, 3), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 3000, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 2000, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 3000, 2), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3000, 1), 1.0f)
             .statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 3000, 0), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 1), 1.0f)
+            .hunger(4)
+            .saturationModifier(1.2f)
             .alwaysEdible()
             .build();
 }
